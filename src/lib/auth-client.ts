e7a8714a05fc443116eb05ee4b8e59a6a6ad1efc
Suprim Khatri <Supremekhatri62@gmail.com>
2026-01-10 12:08:55 +0545

    removed base url from auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,4 @@
 import { createAuthClient } from "better-auth/react";
-console.log("better auth url in client: ", process.env.VITE_BETTER_AUTH_URL);
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.VITE_BETTER_AUTH_URL || "http://localhost:3000",
 });
