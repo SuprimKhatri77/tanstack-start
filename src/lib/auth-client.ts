19928ccf47e1c0000b0315c608568dd88b641a76
Suprim Khatri <Supremekhatri62@gmail.com>
2026-01-10 11:57:53 +0545

    added the logs to check for the base url of the app

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,5 @@
 import { createAuthClient } from "better-auth/react";
+console.log("better auth url: ", process.env.BETTER_AUTH_URL);
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,11 +3,13 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
 
+console.log("better auth url: ", process.env.BETTER_AUTH_URL);
+
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
-  baseURL: process.env.BETTER_AUTH_URL || "localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
 
   emailAndPassword: {
     enabled: true,
