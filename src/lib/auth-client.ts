bfc5ea387f3698b11613c1507777cf4b931e67ec
Suprim Khatri <Supremekhatri62@gmail.com>
2026-01-10 12:05:13 +0545

    added env variables withe vite prefix for client to access it

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
-console.log("better auth url: ", process.env.BETTER_AUTH_URL);
+console.log("better auth url in client: ", process.env.VITE_BETTER_AUTH_URL);
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.VITE_BETTER_AUTH_URL || "http://localhost:3000",
 });
